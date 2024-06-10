10d803e8f0a7dab52994535fe760d10f386a0f62
Jorge Mina <jorge.mina@rappi.com>
2024-06-10 17:31:10 -0500

    Angular 12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,4 +15,4 @@ export const URL_API = 'http://152.200.130.126/gbackend/api';
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
